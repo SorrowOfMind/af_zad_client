66bfaf3a5b77c4f0df4921afb8a4eb40f54f17f5
SorrowOfMind <sortoflifecraze@gmail.com>
2022-01-20 21:39:25 +0100

    topbar

--- client/src/components/TopBar.js
+++ client/src/components/TopBar.js
@@ -2,7 +2,15 @@ import React from 'react';
 
 const TopBar = () => {
   return (
-    <div></div>
+    <header className="header">
+      <nav className="navbar bg-dark navbar-dark ">
+          <div className="container">
+              <a className="navbar-brand" href="#">
+                  ads<span className="header__title-colored-red">fox</span><span className="header__title-colored-teal">__TASK</span>
+              </a>
+          </div>
+      </nav>
+    </header>
   );
 }
 
